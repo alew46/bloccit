dbd2bf2ccca244d1e9a023b5a8a2372568642c83
Alex Lewis <alew46@gmail.com>
2016-07-22 12:33:01 -0400

    Checkpoint 28: completed

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+
+  let(:post) {Post.create!(title:"new Post Title", body: "New Post Body")}
+  let(:comment) {Comment.create!(body: "Comment body", post: post)}
+
+  describe "attributes" do
+    it "has a body attribute" do
+      expect(comment).to have_attributes(body: "Comment body")
+    end
+  end
+
+end
